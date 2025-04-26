c92b783f345041d1f5ae94edcb44695556243ebf
Elhatron <malisz.seweryn@gmail.com>
2025-04-26 08:42:48 +0200

    Created a workspace for Zadanie5

---
+++ Semestr2/Moduł2/Zadanie5/zadanie5.h
@@ -0,0 +1,17 @@
+#ifndef ZADANIE5_H_INCLUDED
+#define ZADANIE5_H_INCLUDED
+
+#include <iostream>
+#include "SewerynMaliszFunctions.h"
+#include <cmath>
+
+using namespace std;
+
+
+void mainOfZadanie()
+{
+}
+
+
+
+#endif // ZADANIE5_H_INCLUDED
